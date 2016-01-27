f973526b850b03ad68eabca6f2b2c5f991776c48
pawan.kumar <pdroid60@gmail.com>
2016-01-27 12:52:52 +0530

    test Git

---
+++ learnpy.py
@@ -0,0 +1,60 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print("Let's talk about %s." % my_name)
+print("He's %d inches tall." % my_height)
+print("He's %d pounds heavy." % my_weight)
+print("Actually that's not too heavy.")
+print("He's got %s eyes and %s hair." % (my_eyes, my_hair))
+print("His teeth are usually %s depending on the coffee." % my_teeth)
+
+# this line is tricky, try to get it exactly right
+print("If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight))
+
+
+x = "There are %d types of people." % 10
+binary = "binary"
+do_not = "don't"
+y = "Those who know %s and those who %s." % (binary, do_not)
+
+print(x)
+print(y)
+
+print("I said: %r." % x)
+print("I also said: '%s'." % y)
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+print(joke_evaluation % hilarious)
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print(w + e)
+print( "Mary had a little lamb.")
+print( "Its fleece was white as %s." % 'snow')
+print( "And everywhere that Mary went.")
+print( "." * 10)  # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end.  try removing it to see what happens
+print(end1 + end2 + end3 + end4 + end5 + end6,)
+print(end7 + end8 + end9 + end10 + end11 + end12)
